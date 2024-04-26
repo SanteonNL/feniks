ce825343e0eb52e5d238302f0e9e10796d40fcd0
Tommy Hetterscheid <t.hetterscheid@santeon.nl>
2024-04-26 17:15:25 +0200

    CSV service

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/SanteonNL/fenix
 
 go 1.19
+
+require github.com/gocarina/gocsv v0.0.0-20231116093920-b87c2d0e983a // indirect
--- main.go
+++ main.go
@@ -2,10 +2,16 @@ package main
 
 import (
 	"bytes"
+	"encoding/csv"
 	"encoding/json"
+	"errors"
 	"fmt"
+	"io"
+	"os"
+	"time"
 
-	"github.com/SanteonNL/fenix/fhir"
+	"github.com/SanteonNL/fenix/models/fhir"
+	"github.com/SanteonNL/fenix/models/sim"
 )
 
 // CSVService is a FHIRService implementation for translating CSV data to FHIR format
@@ -13,7 +19,6 @@ type CSVService struct {
 	FilePath string // Path to the CSV file
 }
 
-// NewCSVService creates a new CSVService with the provided file path
 func NewCSVService(filePath string) *CSVService {
 	return &CSVService{
 		FilePath: filePath,
@@ -21,19 +26,21 @@ func NewCSVService(filePath string) *CSVService {
 }
 
 func main() {
-	// Create a new patient struct
-	id := "123"
-	firstName := "John"
-	lastName := "Doe"
-	birthDate := "2020-01-01"
+	csvService := NewCSVService("input/SIMPatient.csv")
 
-	patient := fhir.Patient{
-		Id:        &id,
-		Name:      []fhir.HumanName{{Given: []string{firstName}, Family: &lastName}},
-		BirthDate: &birthDate,
+	// Get a patient record by ID
+	patient, err := csvService.GetSIMPatientByID("456")
+	if err != nil {
+		fmt.Println("Error getting patient record:", err)
 	}
 
-	jsonBytes, err := json.Marshal(patient)
+	// Translate the patient record to FHIR format
+	fhirPatient, err := TranslateSIMPatientToFHIR(patient)
+	if err != nil {
+		fmt.Println("Error translating SIM patient record:", err)
+	}
+
+	jsonBytes, err := json.Marshal(fhirPatient)
 	if err != nil {
 		fmt.Println("Error marshaling JSON:", err)
 		return
@@ -47,4 +54,57 @@ func main() {
 	}
 
 	fmt.Println(prettyJSON.String())
+}
+func (s *CSVService) GetSIMPatientByID(id string) (*sim.Patient, error) {
+	file, err := os.Open(s.FilePath)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	reader := csv.NewReader(file)
+	reader.Comma = ';'
+
+	// Read and discard the header row
+	if _, err := reader.Read(); err != nil {
+		return nil, err
+	}
+
+	for {
+		record, err := reader.Read()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			return nil, err
+		}
+
+		geboortedatum, _ := time.Parse("2006-01-02", record[6])
+		patient := &sim.Patient{
+			Identificatienummer: &record[0],
+			Geboortedatum:       &geboortedatum, // Parse the BirthDate field as a time.Time,
+			// Continue for the rest of the fields...
+		}
+
+		if *patient.Identificatienummer == id {
+			return patient, nil
+		}
+	}
+
+	return nil, fmt.Errorf("no patient found with ID %s", id)
+}
+
+func TranslateSIMPatientToFHIR(patient *sim.Patient) (*fhir.Patient, error) {
+	if patient == nil {
+		return nil, errors.New("nil patient record")
+	}
+
+	// Translate to FHIR format
+	birthDate := patient.Geboortedatum.Format("2006-01-02") // Format the BirthDate field as a string
+	fhirPatient := &fhir.Patient{
+		Id:        patient.Identificatienummer,
+		BirthDate: &birthDate,
+	}
+
+	return fhirPatient, nil
 }
