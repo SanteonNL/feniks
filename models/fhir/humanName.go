dc810d3b8f197bd7f1fbddd5942915a12f625a0e
Tommy Hetterscheid <t.hetterscheid@santeon.nl>
2024-05-03 16:14:23 +0200

    fix humaName

--- models/fhir/humanName.go
+++ models/fhir/humanName.go
@@ -23,7 +23,7 @@ type HumanName struct {
 	Extension []Extension `bson:"extension,omitempty" json:"extension,omitempty"`
 	Use       *NameUse    `bson:"use,omitempty" json:"use,omitempty"`
 	Text      *string     `bson:"text,omitempty" json:"text,omitempty"`
-	    *string     `bson:"family,omitempty" json:"family,omitempty"`
+	Family    *string     `bson:"family,omitempty" json:"family,omitempty"`
 	Given     []string    `bson:"given,omitempty" json:"given,omitempty"`
 	Prefix    []string    `bson:"prefix,omitempty" json:"prefix,omitempty"`
 	Suffix    []string    `bson:"suffix,omitempty" json:"suffix,omitempty"`
