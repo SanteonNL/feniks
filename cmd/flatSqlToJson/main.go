5f71109ecb0a264861d1beafac2621f6830b4416
Tommy Hetterscheid <t.hetterscheid@santeon.nl>
2024-07-15 17:04:53 +0200

    working version

--- cmd/flatSqlToJson/main.go
+++ cmd/flatSqlToJson/main.go
@@ -1,11 +1,13 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"os"
 	"reflect"
 	"strconv"
 	"strings"
+	"time"
 
 	"github.com/SanteonNL/fenix/models/fhir"
 	"github.com/jmoiron/sqlx"
@@ -15,16 +17,18 @@ import (
 )
 
 type RowData map[string]interface{}
+type SearchParameter struct {
+	FieldName string
+	Value     interface{}
+}
 
 func main() {
-
-	l := zerolog.New(zerolog.MultiLevelWriter(zerolog.ConsoleWriter{Out: os.Stdout})).With().Timestamp().Caller().Logger()
-
-	l.Debug().Msg("Starting flatSqlToJson")
+	logger := zerolog.New(zerolog.ConsoleWriter{Out: os.Stdout}).With().Timestamp().Caller().Logger()
+	logger.Debug().Msg("Starting flatSqlToJson")
 
 	db, err := sqlx.Connect("postgres", "postgres://postgres:mysecretpassword@localhost:5432/tsl_employee?sslmode=disable")
 	if err != nil {
-		fmt.Println(err)
+		log.Fatal().Err(err).Msg("Failed to connect to the database")
 		return
 	}
 
@@ -32,47 +36,42 @@ func main() {
 
 	err = PopulateStructs(db, &patient)
 	if err != nil {
-		fmt.Println(err)
+		log.Fatal().Err(err).Msg("Failed to populate struct")
 		return
 	}
 
-	//fmt.Printf("%+v\n", patient)
+	jsonData, err := json.MarshalIndent(patient, "", "  ")
+	if err != nil {
+		log.Fatal().Err(err).Msg("Failed to marshal patient to JSON")
+		return
+	}
 
+	fmt.Println("JSON data:")
+	fmt.Println(string(jsonData))
 }
 
 func PopulateStructs(db *sqlx.DB, s interface{}) error {
 	query := `
-    SELECT
-    'Patient' as field_name,
-    '' as parent_id,
-    p.identificatienummer id,
-    -- CASE
-    --     WHEN p.geslachtcode = 'M' THEN 'male'
-    --     WHEN p.geslachtcode = 'F' THEN 'female'
-    --     ELSE 'unknown'
-    -- END as gender, 
-    p.geboortedatum birthdate
-    FROM patient p
-WHERE 1=1
-AND p.identificatienummer = '123';
+		SELECT
+			'Patient' as field_name,
+			'' as parent_id,
+			p.identificatienummer id,
+			p.geboortedatum birthdate
+		FROM patient p
+		WHERE p.identificatienummer = '123';
 
+		SELECT
+			'Patient.Name' as field_name,
+			p.identificatienummer as parent_id,
+			concat(p.identificatienummer, humanName.lastname) AS id,
+			humanName.lastname as family,
+			humanName.firstname AS name
+		FROM patient p
+		JOIN names humanName ON humanName.identificatienummer = p.identificatienummer
+		WHERE p.identificatienummer = '123'
+		GROUP BY p.identificatienummer, humanName.lastname, humanName.firstname;
+	`
 
-SELECT
-    'Patient.Name' as field_name,
-    p.identificatienummer as parent_id,
-    concat(p.identificatienummer,humanName.lastname) AS id,
-    humanName.lastname as family,
-    humanName.firstname AS name
-FROM
-    patient p
-    JOIN names humanName ON humanName.identificatienummer = p.identificatienummer
-WHERE 1=1
- AND p.identificatienummer = '123'   
-GROUP BY
-    p.identificatienummer, humanName.lastname,humanName.firstname;
-   `
-
-	//patientID := "123"
 	rows, err := db.Queryx(query)
 	if err != nil {
 		return err
@@ -92,7 +91,6 @@ GROUP BY
 			continue
 		}
 
-		// Scan the entire row into a map
 		rowData := make(RowData)
 		err = rows.MapScan(rowData)
 		if err != nil {
@@ -106,71 +104,137 @@ GROUP BY
 			resultMap[fieldName] = make(map[string][]RowData)
 		}
 		resultMap[fieldName][parentID] = append(resultMap[fieldName][parentID], rowData)
-
-		//	log.Debug().Msgf("fieldName: %s, parentID: %s, rowData: %v", fieldName, parentID, rowData)
 	}
 
-	err = populateStruct(s, resultMap, "")
-	if err != nil {
-		return err
+	v := reflect.ValueOf(s).Elem()
+	return populateStruct(v, resultMap, "")
+}
+
+func populateStruct(v reflect.Value, resultMap map[string]map[string][]RowData, parentField string) error {
+	if v.Kind() != reflect.Struct {
+		return fmt.Errorf("value is not a struct")
+	}
+
+	structName := v.Type().Name()
+	fullFieldName := parentField
+
+	if parentField == "" {
+		fullFieldName = structName
+	}
+
+	// Check if there's data for this struct
+	if data, ok := resultMap[fullFieldName]; ok {
+		for _, rows := range data {
+			for _, row := range rows {
+				err := populateStructFromRow(v.Addr().Interface(), row)
+				if err != nil {
+					return err
+				}
+			}
+		}
+	}
+
+	// Recursively populate nested fields
+	for i := 0; i < v.NumField(); i++ {
+		field := v.Field(i)
+		fieldName := v.Type().Field(i).Name
+		newFullFieldName := fullFieldName + "." + fieldName
+
+		// Check if this field or any of its nested fields exist in the resultMap
+		if fieldExistsInResultMap(resultMap, newFullFieldName) {
+			err := populateField(field, resultMap, newFullFieldName)
+			if err != nil {
+				return err
+			}
+		}
 	}
 
 	return nil
 }
 
-func populateStruct(s interface{}, resultMap map[string]map[string][]RowData, parentPath string) error {
-	v := reflect.ValueOf(s).Elem()
+func fieldExistsInResultMap(resultMap map[string]map[string][]RowData, fieldName string) bool {
+	// Check if the field itself exists
+	if _, ok := resultMap[fieldName]; ok {
+		return true
+	}
+
+	// Check if any nested fields exist
+	for key := range resultMap {
+		if strings.HasPrefix(key, fieldName+".") {
+			return true
+		}
+	}
+	return false
+}
+
+func populateField(field reflect.Value, resultMap map[string]map[string][]RowData, fieldName string) error {
+	log.Debug().Msgf("Populating field in populateField: %s", fieldName)
+	switch field.Kind() {
+	case reflect.Slice:
+		return populateSlice(field, resultMap, fieldName)
+	case reflect.Struct:
+		return populateStruct(field, resultMap, fieldName)
+	case reflect.Ptr:
+		if field.IsNil() {
+			field.Set(reflect.New(field.Type().Elem()))
+		}
+		return populateField(field.Elem(), resultMap, fieldName)
+	default:
+		return populateBasicType(field, resultMap, fieldName)
+	}
+}
+
+func populateBasicType(field reflect.Value, resultMap map[string]map[string][]RowData, fullFieldName string) error {
+	log.Debug().Msgf("Populating basic type fullFieldName: %s", fullFieldName)
+
+	data, ok := resultMap[fullFieldName]
+	if !ok {
+		return nil // No data for this field, skip it
+	}
+
+	fieldName := strings.Split(fullFieldName, ".")[len(strings.Split(fullFieldName, "."))-1]
+	log.Debug().Msgf("Populating basic type fieldName: %s", fieldName)
+
+	for _, rows := range data {
+		for _, row := range rows {
+			for key, value := range row {
+				if strings.EqualFold(key, fieldName) {
+					log.Debug().Msgf("Setting field: %s with value: %v", fieldName, value)
+					return SetField(field.Addr().Interface(), fieldName, value)
+				}
+			}
+		}
+	}
+	return nil
+}
+
+func populateSlice(field reflect.Value, resultMap map[string]map[string][]RowData, fieldName string) error {
+	if data, ok := resultMap[fieldName]; ok {
+		for _, rows := range data {
+			for _, row := range rows {
+				newElem := reflect.New(field.Type().Elem()).Elem()
+				err := populateStructFromRow(newElem.Addr().Interface(), row)
+				if err != nil {
+					return err
+				}
+				field.Set(reflect.Append(field, newElem))
+			}
+		}
+	}
+	return nil
+}
+
+func populateStructFromRow(obj interface{}, row RowData) error {
+	v := reflect.ValueOf(obj).Elem()
 	t := v.Type()
 
-	structName := reflect.TypeOf(s).Elem().Name()
-
-	for i := 0; i < t.NumField(); i++ {
-		field := t.Field(i)
-		fieldName := field.Name
-
-		if parentPath == "" {
-			parentPath = structName
-		}
-
-		//fieldPath := parentPath + "." + fieldName
-
-		//log.Debug().Msgf("fieldName before if: %s", fieldName)
-
-		if data, ok := resultMap[structName]; ok {
-			//log.Debug().Msgf("fieldPath: %s", fieldPath)
-
-			parentID := ""
-			if idField := v.FieldByName("ID"); idField.IsValid() {
-				parentID = idField.String()
-			}
-
-			log.Debug().Msgf("parentID: %s, data[] %v", parentID, data[parentID])
-
-			if rowsSlice, ok := data[parentID]; ok && len(rowsSlice) == 1 {
-				fieldValue := v.Field(i)
-				log.Debug().Msgf("fieldName %s, fieldValue: %v, rowSlice %v", fieldName, fieldValue, rowsSlice)
-				err := populateStructFromRow(fieldValue, fieldName, rowsSlice[0])
-				if err != nil {
-					return err
-				}
-			}
-		}
-	}
-
-	return nil
-}
-
-func populateStructFromRow(fieldValue reflect.Value, fieldName string, row RowData) error {
 	for key, value := range row {
-		log.Debug().Msgf("key: %s, fieldName %s, value: %v", key, fieldName, value)
-		fieldNameLower := strings.ToLower(fieldName)
-		if fieldNameLower == key {
-			log.Debug().Msgf("Setting value for field %s: %v", fieldName, value)
-			if fieldValue.Kind() == reflect.Ptr {
-				fieldValue = fieldValue.Elem()
-			}
-			if field := fieldValue.FieldByName(key); field.IsValid() && field.CanSet() {
-				err := setFieldValue(field, value)
+		for i := 0; i < t.NumField(); i++ {
+			field := t.Field(i)
+			fieldNameLower := strings.ToLower(field.Name)
+
+			if fieldNameLower == strings.ToLower(key) {
+				err := SetField(obj, field.Name, value)
 				if err != nil {
 					return err
 				}
@@ -180,30 +244,76 @@ func populateStructFromRow(fieldValue reflect.Value, fieldName string, row RowDa
 	return nil
 }
 
-func setFieldValue(field reflect.Value, value interface{}) error {
-	switch field.Kind() {
-	case reflect.String:
-		field.SetString(value.(string))
-	case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
-		intValue, err := strconv.ParseInt(value.(string), 10, 64)
-		if err != nil {
-			return err
-		}
-		field.SetInt(intValue)
-	case reflect.Float32, reflect.Float64:
-		floatValue, err := strconv.ParseFloat(value.(string), 64)
-		if err != nil {
-			return err
-		}
-		field.SetFloat(floatValue)
-	case reflect.Bool:
-		boolValue, err := strconv.ParseBool(value.(string))
-		if err != nil {
-			return err
-		}
-		field.SetBool(boolValue)
-	default:
-		return fmt.Errorf("unsupported field type: %v", field.Kind())
+func SetField(obj interface{}, name string, value interface{}) error {
+	structValue := reflect.ValueOf(obj)
+	if structValue.Kind() != reflect.Ptr || structValue.IsNil() {
+		return fmt.Errorf("obj must be a non-nil pointer to a struct")
 	}
+
+	structElem := structValue.Elem()
+	if structElem.Kind() != reflect.Struct {
+		return fmt.Errorf("obj must point to a struct")
+	}
+
+	field := structElem.FieldByName(name)
+	if !field.IsValid() {
+		return fmt.Errorf("no such field: %s in obj", name)
+	}
+
+	if !field.CanSet() {
+		return fmt.Errorf("cannot set field %s", name)
+	}
+
+	fieldValue := reflect.ValueOf(value)
+
+	if field.Kind() == reflect.Ptr && (field.Type().Elem().Kind() == reflect.String || field.Type().Elem().Kind() == reflect.Bool) {
+		var newValue reflect.Value
+
+		switch field.Type().Elem().Kind() {
+		case reflect.String:
+			var strValue string
+			switch v := value.(type) {
+			case string:
+				strValue = v
+			case int, int8, int16, int32, int64:
+				strValue = fmt.Sprintf("%d", v)
+			case uint, uint8, uint16, uint32, uint64:
+				strValue = fmt.Sprintf("%d", v)
+			case float32, float64:
+				strValue = fmt.Sprintf("%f", v)
+			case bool:
+				strValue = strconv.FormatBool(v)
+			case time.Time:
+				strValue = v.Format(time.RFC3339)
+			default:
+				return fmt.Errorf("cannot convert %T to *string", value)
+			}
+			newValue = reflect.ValueOf(&strValue)
+		case reflect.Bool:
+			var boolValue bool
+			switch v := value.(type) {
+			case bool:
+				boolValue = v
+			case string:
+				var err error
+				boolValue, err = strconv.ParseBool(v)
+				if err != nil {
+					return fmt.Errorf("cannot convert string to *bool: %s", v)
+				}
+			default:
+				return fmt.Errorf("cannot convert %T to *bool", value)
+			}
+			newValue = reflect.ValueOf(&boolValue)
+		}
+
+		field.Set(newValue)
+	} else {
+		if field.Type() != fieldValue.Type() {
+			return fmt.Errorf("provided value type didn't match obj field type")
+		}
+
+		field.Set(fieldValue)
+	}
+
 	return nil
 }
