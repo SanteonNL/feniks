1651192ca80b30df199aac43f474b809e0287b51
Leny van Wijk <l.vanwijk@santeon.nl>
2024-10-11 14:47:30 +0200

    this does not work

--- cmd/flatSqlToJson/main.go
+++ cmd/flatSqlToJson/main.go
@@ -524,27 +524,13 @@ func SetField(structPath string, structPointer interface{}, structFieldName stri
 	// Try UnmarshalJSON for the field and its address
 	for _, field := range []reflect.Value{structField, structField.Addr()} {
 		if field.CanInterface() && field.Type().Implements(reflect.TypeOf((*json.Unmarshaler)(nil)).Elem()) {
-			// Check if the field is a pointer to a type that implements UnmarshalJSON
-			if structField.Kind() == reflect.Ptr {
-				if structField.IsNil() {
-					structField.Set(reflect.New(structField.Type().Elem()))
-				}
-			}
-			//unmarshalJSONMethod := structField.MethodByName("UnmarshalJSON")
-			//if unmarshalJSONMethod.IsValid() {
-			/*jsonInputValue, err := json.Marshal(inputValue)
-			if err != nil {
-				return fmt.Errorf("failed to marshal value to JSON: %v", err)
-			}*/
 			byteValue, err := getByteValue(inputValue)
 			if err != nil {
 				return fmt.Errorf("failed to convert input to []byte: %v", err)
 			}
-			// Call UnmarshalJSON
-			method := structField.MethodByName("UnmarshalJSON")
-			results := method.Call([]reflect.Value{reflect.ValueOf(byteValue)})
 
-			//results := unmarshalJSONMethod.Call([]reflect.Value{reflect.ValueOf(jsonInputValue)})
+			method := field.MethodByName("UnmarshalJSON")
+			results := method.Call([]reflect.Value{reflect.ValueOf(byteValue)})
 			if len(results) > 0 && !results[0].IsNil() {
 				return results[0].Interface().(error)
 			}
