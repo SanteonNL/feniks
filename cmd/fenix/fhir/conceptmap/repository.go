05097050fa17f6cf52f81e38c5cd06c7ea203f98
Leny van Wijk <l.vanwijk@santeon.nl>
2025-01-13 22:54:25 +0100

    renamed functions, added todo's, added debugging logs, used existing function instead of duplicate code, added function to check cache,

--- cmd/fenix/fhir/conceptmap/repository.go
+++ cmd/fenix/fhir/conceptmap/repository.go
@@ -12,6 +12,7 @@ import (
 	"github.com/rs/zerolog"
 )
 
+// TODO add source or target in function names to make it more clear what the function does
 // ConceptMapRepository handles loading and storing ConceptMap resources.
 type ConceptMapRepository struct {
 	log         zerolog.Logger
@@ -29,8 +30,13 @@ func NewConceptMapRepository(localPath string, log zerolog.Logger) *ConceptMapRe
 	}
 }
 
-// LoadConceptMaps loads all ConceptMaps into the repository.
-func (repo *ConceptMapRepository) LoadConceptMaps() error {
+// LoadConceptMapsIntoRepository loads all ConceptMaps into the repository.
+func (repo *ConceptMapRepository) LoadConceptMapsIntoRepository() error {
+	if _, err := os.Stat(repo.localPath); os.IsNotExist(err) {
+		repo.log.Warn().Str("path", repo.localPath).Msg("ConceptMap directory does not exist")
+		return nil
+	}
+
 	files, err := os.ReadDir(repo.localPath)
 	if err != nil {
 		repo.log.Error().Err(err).Msg("Failed to read directory")
@@ -42,7 +48,7 @@ func (repo *ConceptMapRepository) LoadConceptMaps() error {
 			filePath := filepath.Join(repo.localPath, file.Name())
 			repo.log.Debug().Str("filePath", filePath).Msg("Loading ConceptMap file")
 
-			conceptMap, err := repo.loadConceptMapFile(filePath)
+			conceptMap, err := repo.readConceptMapFromFile(filePath)
 			if err != nil {
 				repo.log.Error().
 					Err(err).
@@ -63,10 +69,12 @@ func (repo *ConceptMapRepository) LoadConceptMaps() error {
 			}
 
 			if conceptMap.TargetUri != nil {
-				repo.cache.Store(*conceptMap.TargetUri, conceptMap)
-				repo.log.Debug().
-					Str("targetUri", *conceptMap.TargetUri).
-					Msg("Loaded ConceptMap into cache by TargetUri")
+				if *conceptMap.TargetUri != "" {
+					repo.cache.Store(*conceptMap.TargetUri, conceptMap)
+					repo.log.Debug().
+						Str("targetUri", *conceptMap.TargetUri).
+						Msg("Loaded ConceptMap into cache by TargetUri")
+				}
 			} else {
 				repo.log.Warn().
 					Str("file", file.Name()).
@@ -80,9 +88,10 @@ func (repo *ConceptMapRepository) LoadConceptMaps() error {
 }
 
 // loadConceptMapFile loads a ConceptMap from a file.
-func (repo *ConceptMapRepository) loadConceptMapFile(filePath string) (*fhir.ConceptMap, error) {
+func (repo *ConceptMapRepository) readConceptMapFromFile(filePath string) (*fhir.ConceptMap, error) {
 	data, err := os.ReadFile(filePath)
 	if err != nil {
+		// TODO check of tis should be fatal or Error (in the GetOrLoadConceptMap function it is an error)
 		repo.log.Fatal().Err(err).Str("path", filePath).Msg("Failed to read ConceptMap file")
 		return nil, fmt.Errorf("failed to read ConceptMap file: %w", err)
 	}
@@ -95,14 +104,21 @@ func (repo *ConceptMapRepository) loadConceptMapFile(filePath string) (*fhir.Con
 	return &conceptMap, nil
 }
 
-// GetConceptMap retrieves a ConceptMap by ID or URL.
-func (repo *ConceptMapRepository) GetConceptMap(url string) (*fhir.ConceptMap, error) {
+// GetOrLoadConceptMap retrieves a ConceptMap by URL, loading it from disk if not already in cache.
+func (repo *ConceptMapRepository) GetOrLoadConceptMap(url string) (*fhir.ConceptMap, error) {
 
 	// Try cache first
 	if cached, ok := repo.cache.Load(url); ok {
 		return cached.(*fhir.ConceptMap), nil
 	}
 
+	// Load from disk if not in cache
+	// TOOD: Ask why this is needed; because the cache should be loaded with all ConceptMaps if first in the main all conceptmaps
+	// are loaded from disk and next all converted conceptmaps are also loaded from disk
+	// It might however be usefull if you do not want to load all conceptmaps at once but only the ones you need (and load them only once)
+	// Also the function GetConceptMapFileNameByURL uses readConceptMapFromFile  so it seems a bit circular...
+	// Beacuse it needs to read the conceptmap to determine the filename. Soe maybe jsut let GetConceptMapFileNameByURL
+	// return the conceptmap and not the filename
 	fileName, err := repo.GetConceptMapFileNameByURL(url)
 	if err != nil {
 		return nil, err
@@ -110,46 +126,47 @@ func (repo *ConceptMapRepository) GetConceptMap(url string) (*fhir.ConceptMap, e
 
 	filePath := filepath.Join(repo.localPath, fileName)
 
-	data, err := os.ReadFile(filePath)
+	conceptMap, err := repo.readConceptMapFromFile(filePath)
 	if err != nil {
-		repo.log.Error().Err(err).Str("path", filePath).Msg("Failed to read ConceptMap file")
-		return nil, fmt.Errorf("failed to read ConceptMap file: %w", err)
+		repo.cache.Store(url, conceptMap)
+		return conceptMap, nil
+	} else {
+		return nil, err
 	}
-
-	var conceptMap fhir.ConceptMap
-	if err := json.Unmarshal(data, &conceptMap); err != nil {
-		return nil, fmt.Errorf("failed to parse ConceptMap: %w", err)
-	}
-	repo.cache.Store(url, &conceptMap)
-	return &conceptMap, nil
 }
 
-// GetConceptMapsByValuesetURL retrieves all ConceptMaps with a target URI matching the input URL.
-func (repo *ConceptMapRepository) GetConceptMapsByValuesetURL(valueSetURL string) ([]string, error) {
+// GetConceptMapURLsByValuesetURL retrieves all ConceptMaps with a target URI matching the input URL.
+func (repo *ConceptMapRepository) GetConceptMapURLsByValuesetURL(valueSetURL string) ([]string, error) {
 	var matchingConceptMapURLs []string
 
 	repo.cache.Range(func(key, value interface{}) bool {
+		repo.log.Debug().Str("key", key.(string)).Msg("Checking ConceptMap for ValueSet URL")
 		conceptMap := value.(*fhir.ConceptMap)
 		if conceptMap.TargetUri != nil && *conceptMap.TargetUri == valueSetURL {
+			repo.log.Debug().Str("Adding ConceptMap URL", *conceptMap.Url).Msg("Found matching ConceptMap")
 			matchingConceptMapURLs = append(matchingConceptMapURLs, *conceptMap.Url)
 		}
 		return true
 	})
 
 	if len(matchingConceptMapURLs) == 0 {
-		repo.log.Warn().Str("valueSetURL", valueSetURL).Msg("No ConceptMaps found for ValueSet URL")
-		return nil, fmt.Errorf("no ConceptMaps found for ValueSet URL: %s", valueSetURL)
+		repo.log.Warn().Str("valueSetURL", valueSetURL).Msg("No ConceptMapURLs found for ValueSet URL")
+		return nil, fmt.Errorf("no ConceptMapURLs found for ValueSet URL: %s", valueSetURL)
 	}
 
 	return matchingConceptMapURLs, nil
 }
 
+// TODO check if this function is really needed, it seems not to be used anywhere
 // Helper function to get the file name for a ConceptMap by ID or URL.
 func (repo *ConceptMapRepository) getFileName(key string) string {
 	return fmt.Sprintf("%s.json", key)
 }
 
 // GetConceptMapFileNameByURL returns the filename of a ConceptMap based on its URL
+// This function is only called in the  GetOrLoadConceptMap function but I am not sure it is really needed there
+// It might however be usefull if you do not want to have all conceptmaps loaded at once but only the ones you need
+// TODO check if this function is really neededs
 func (repo *ConceptMapRepository) GetConceptMapFileNameByURL(url string) (string, error) {
 	var matchingFileName string
 	err := filepath.Walk(repo.localPath, func(path string, info os.FileInfo, err error) error {
@@ -162,7 +179,7 @@ func (repo *ConceptMapRepository) GetConceptMapFileNameByURL(url string) (string
 			return nil
 		}
 
-		conceptMap, err := repo.loadConceptMapFile(path)
+		conceptMap, err := repo.readConceptMapFromFile(path)
 		if err != nil {
 			repo.log.Warn().
 				Err(err).
@@ -172,6 +189,10 @@ func (repo *ConceptMapRepository) GetConceptMapFileNameByURL(url string) (string
 			return nil // Continue walking even if one file fails
 		}
 
+		// TODO check if the targetUri check is a remnant of artdecor and should be removed?
+		// As targeturi should be the url of the value set and not the url of the conceptmap...
+		// TODO: check implications of stopping the walk when a match is found
+		// Supposedly url names are unique so it should be fine, but do we make sure url names are unique?
 		// Check both URL and TargetUri
 		if (conceptMap.Url != nil && *conceptMap.Url == url) ||
 			(conceptMap.TargetUri != nil && *conceptMap.TargetUri == url) {
@@ -195,4 +216,17 @@ func (repo *ConceptMapRepository) GetConceptMapFileNameByURL(url string) (string
 	}
 
 	return matchingFileName, nil
+}
+
+// GetAllConceptMapsFromCache returns all ConceptMaps in the repository, this is just used to check what is in cache
+// and not really needed functionality
+func (r *ConceptMapRepository) GetAllConceptMapsFromCache() []string {
+	var keys []string
+	r.cache.Range(func(key, value interface{}) bool {
+		keys = append(keys, key.(string))
+		return true
+	})
+
+	return keys
+
 }
